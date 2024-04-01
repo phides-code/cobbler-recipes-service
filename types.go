73d5d6baf16bbbc201c71f9603826c2c54490fb7
Philippe DeSousa <phil.desousa@gmail.com>
2024-03-31 21:04:26 -0400

    update types

--- types.go
+++ types.go
@@ -16,7 +16,7 @@ type Entity struct {
 	AuthorId    string       `json:"authorId" dynamodbav:"authorId"`
 	Title       string       `json:"title" dynamodbav:"title"`
 	Description string       `json:"description" dynamodbav:"description"`
-	FoodType    string       `json:"foodType" dynamodbav:"foodType"`
+	FoodType    []string     `json:"foodType" dynamodbav:"foodType"`
 	Cuisine     string       `json:"cuisine" dynamodbav:"cuisine"`
 	Ingredients []Ingredient `json:"ingredients" dynamodbav:"ingredients"`
 	Steps       []Step       `json:"steps" dynamodbav:"steps"`
@@ -27,7 +27,7 @@ type NewOrUpdatedEntity struct {
 	AuthorId    string       `json:"authorId" validate:"required"`
 	Title       string       `json:"title" validate:"required"`
 	Description string       `json:"description" validate:"required"`
-	FoodType    string       `json:"foodType" validate:"required"`
+	FoodType    []string     `json:"foodType" validate:"required"`
 	Cuisine     string       `json:"cuisine" validate:"required"`
 	Ingredients []Ingredient `json:"ingredients" validate:"required"`
 	Steps       []Step       `json:"steps" validate:"required"`
